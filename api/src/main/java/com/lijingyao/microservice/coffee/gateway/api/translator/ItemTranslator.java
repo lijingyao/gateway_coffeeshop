7b6e2648cc6640276aaa04002fb78ec06b5bb78d
weiwen <weiwen@zb100.com>
2018-07-09 21:31:36 +0800

    update translators

--- api/src/main/java/com/lijingyao/microservice/coffee/gateway/api/translator/ItemTranslator.java
+++ api/src/main/java/com/lijingyao/microservice/coffee/gateway/api/translator/ItemTranslator.java
@@ -2,8 +2,14 @@ package com.lijingyao.microservice.coffee.gateway.api.translator;
 
 import com.lijingyao.microservice.coffee.gateway.api.base.BaseTranslator;
 import com.lijingyao.microservice.coffee.gateway.api.translator.clients.ItemClient;
+import com.lijingyao.microservice.coffee.template.items.OrderItemDTO;
+import com.lijingyao.microservice.coffee.template.items.OrderItemPriceDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import rx.Observable;
+import rx.Subscriber;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by lijingyao on 2018/7/9 18:56.
@@ -15,6 +21,18 @@ public class ItemTranslator extends BaseTranslator {
     private ItemClient itemClient;
 
 
+
+    private OrderItemPriceDTO buildItemOrderPrice(OrderItemDTO orderItemDTO) {
+        ResponseEntity result = itemClient.buildItemOrderPrice(orderItemDTO);
+        return (OrderItemPriceDTO) valueOfResult(result, OrderItemPriceDTO.class);
+    }
+
+    public Observable<OrderItemPriceDTO> buildItemOrderPriceObs(OrderItemDTO orderItemDTO) {
+        return Observable.unsafeCreate((Subscriber<? super OrderItemPriceDTO> s) -> {
+            s.onNext(buildItemOrderPrice(orderItemDTO));
+            s.onCompleted();
+        }).subscribeOn(Schedulers.io());
+    }
 
 
 }
--- api/src/main/java/com/lijingyao/microservice/coffee/gateway/api/translator/OrderTranslator.java
+++ api/src/main/java/com/lijingyao/microservice/coffee/gateway/api/translator/OrderTranslator.java
@@ -2,8 +2,16 @@ package com.lijingyao.microservice.coffee.gateway.api.translator;
 
 import com.lijingyao.microservice.coffee.gateway.api.base.BaseTranslator;
 import com.lijingyao.microservice.coffee.gateway.api.translator.clients.OrderClient;
+import com.lijingyao.microservice.coffee.template.trade.OrderCreateDTO;
+import com.lijingyao.microservice.coffee.template.trade.OrderDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import rx.Observable;
+import rx.Subscriber;
+import rx.schedulers.Schedulers;
+
+import java.util.List;
 
 /**
  * Created by lijingyao on 2018/7/9 18:56.
@@ -13,6 +21,31 @@ public class OrderTranslator extends BaseTranslator {
 
     @Autowired
     private OrderClient orderClient;
+
+
+    private OrderDTO orderCreative(OrderCreateDTO orderCreateDTO) {
+        ResponseEntity result = orderClient.orderCreative(orderCreateDTO);
+        return (OrderDTO) valueOfResult(result, OrderDTO.class);
+    }
+
+    public Observable<OrderDTO> orderCreativeObs(OrderCreateDTO orderCreateDTO) {
+        return Observable.unsafeCreate((Subscriber<? super OrderDTO> s) -> {
+            s.onNext(orderCreative(orderCreateDTO));
+            s.onCompleted();
+        }).subscribeOn(Schedulers.io());
+    }
+
+    private List<OrderDTO> getUserNewOrders(Long userId, Integer mainOrderSize, Integer detailShowSize) {
+        ResponseEntity result = orderClient.getUserNewOrders(userId, mainOrderSize, detailShowSize);
+        return (List<OrderDTO>) valueOfResult(result, OrderDTO.class);
+    }
+
+    public Observable<List<OrderDTO>> getUserNewOrdersObs(Long userId, Integer mainOrderSize, Integer detailShowSize) {
+        return Observable.unsafeCreate((Subscriber<? super List<OrderDTO>> s) -> {
+            s.onNext(getUserNewOrders(userId, mainOrderSize, detailShowSize));
+            s.onCompleted();
+        }).subscribeOn(Schedulers.io());
+    }
 
 
 }
